2df341e866c632d646e3de3fc4fcf641ff11133b
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-06-10 21:56:20 +0530

    Made explicit nested routes spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     resources :companies do
-      resources :transactions
+      resources :transactions, only: [:new, :create]
     end
   root "companies#index"
 end
